84b213e2ea7666378bd8bf708f65c1b0928b32d0
DrKillex <lcollaro1994@gmail.com>
2023-03-21 16:20:27 +0100

    completo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-import { createApp } from 'vue'
+const { createApp } = Vue
 
 createApp({
     data() {
         return {
             emailList: [],
         }
+    },
+    methods: {
+        
+    },
+    created() {
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {                
+                console.log(response);
+                this.emailList.push(response.data.response)
+            })
+        }       
     }
 }).mount('#app')
+
+
